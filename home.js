af6bcc4bc3f4d2d779990e888d03e1f3cddee22c
SaiTejaswani123 <117744898+SaiTejaswani123@users.noreply.github.com>
2023-04-10 14:01:16 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   constructor(props) {
@@ -101,3 +102,18 @@ class Home extends Component {
     </div>
   );
 }
+
+class TrendingSubjects extends Component {
+  // ...
+  render() {
+    return (
+      <div>
+        <h1>Trending Subjects</h1>
+        <Link to="/">Back to Home</Link>
+        {/* ... */}
+      </div>
+    );
+  }
+}
+
+export default TrendingSubjects;
